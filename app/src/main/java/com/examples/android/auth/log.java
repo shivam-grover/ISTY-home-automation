db9272cf912a5ca0d6ba914918dfe0627734c382
shivumgrover <shivumgrover99@gmail.com>
2019-04-13 20:08:55 +0530

    Add final files

---
+++ app/src/main/java/com/examples/android/auth/log.java
@@ -0,0 +1,18 @@
+package com.examples.android.auth;
+
+public class log {
+    String email, typel;
+
+    public log(String email, String typel) {
+        this.email = email;
+        this.typel = typel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTypel() {
+        return typel;
+    }
+}
